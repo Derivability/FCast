5eea46d540714559350bf82d4b0e7e4982cbf09e
Derivability <derivability@wraithgod.strangled.net>
2025-10-27 12:08:35 +0200

    Add EventToType and reverse mappings
    Signed-off-by: Derivability <derivability@wraithgod.strangled.net>

--- fcast/utils.py
+++ fcast/utils.py
@@ -1,24 +1,38 @@
-from .opcodes import Opcodes
+from .opcode import Opcode
 from .message import *
+from .event_type import EventType
+from .event import *
 
 
-OpcodesToMessages = {
-    Opcodes.play: Play,
-    Opcodes.pause: Pause,
-    Opcodes.resume: Resume,
-    Opcodes.stop: Stop,
-    Opcodes.seek: Seek,
-    Opcodes.playback_update: PlaybackUpdate,
-    Opcodes.volume_update: VolumeUpdate,
-    Opcodes.set_volume: SetVolume,
-    Opcodes.playback_error: PlaybackError,
-    Opcodes.set_speed: SetSpeed,
-	Opcodes.version: Version,
-    Opcodes.ping: Ping,
-    Opcodes.pong: Pong,
-	Opcodes.initial: Initial,
-    Opcodes.play_update: PlayUpdate,
-    Opcodes.set_playlist_item: SetPlaylistItem
+OpcodeToMessage = {
+    Opcode.play: Play,
+    Opcode.pause: Pause,
+    Opcode.resume: Resume,
+    Opcode.stop: Stop,
+    Opcode.seek: Seek,
+    Opcode.playback_update: PlaybackUpdate,
+    Opcode.volume_update: VolumeUpdate,
+    Opcode.set_volume: SetVolume,
+    Opcode.playback_error: PlaybackError,
+    Opcode.set_speed: SetSpeed,
+	Opcode.version: Version,
+    Opcode.ping: Ping,
+    Opcode.pong: Pong,
+	Opcode.initial: Initial,
+    Opcode.play_update: PlayUpdate,
+    Opcode.set_playlist_item: SetPlaylistItem,
+    Opcode.event: EventM
 }
 
-MessagesToOpcodes = {v:k for k,v in OpcodesToMessages.items()}
+MessageToOpcode = {v:k for k,v in OpcodeToMessage.items()}
+
+
+TypeToEvent = {
+    EventType.MediaItemStart: MediaItemStart,
+    EventType.MediaItemEnd: MediaItemEnd,
+    EventType.MediaItemChange: MediaItemChange,
+    EventType.KeyDown: KeyDown,
+    EventType.KeyUp: KeyUp
+}
+
+EventToType = {v:k for k,v in TypeToEvent.items()}
